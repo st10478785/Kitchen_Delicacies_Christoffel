5ff97436d1e79fe2fd47b5701b0a6ec06a15921a
st10478785 <st10478785@imconnect.edu.za>
2025-10-08 14:49:06 +0200

    App (commit 5)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Imports 
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Import screens
+import SplashScreen from './screens/SplashScreen';
+import HomeScreenG from './screens/HomeScreenG';
+import HomeScreenA from './screens/HomeScreenA';
+
+export type RootStackParamList = {
+  Splash: undefined;
+  HomeG: undefined;
+  HomeA: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="HomeG" component={HomeScreenG} />
+        <Stack.Screen name="HomeA" component={HomeScreenA} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
